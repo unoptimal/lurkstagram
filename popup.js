1387c405901b5e5e0c2298461ab4b36416e7ca7e
unoptimal <unoptimalblogs@gmail.com>
2024-10-20 11:26:12 -0700

    stuff

--- popup.js
+++ popup.js
@@ -3,10 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var toggleSwitch = document.getElementById('toggleSwitch');
     var statusElement = document.getElementById('status');
     var slider = document.querySelector('.slider');
+    var body = document.body;
     
     function updateStatus(isEnabled) {
       statusElement.textContent = isEnabled ? 'ENABLED' : 'DISABLED';
       statusElement.className = 'status ' + (isEnabled ? 'enabled' : 'disabled');
+      
+      if (isEnabled) {
+        body.classList.add('dark-mode');
+      } else {
+        body.classList.remove('dark-mode');
+      }
     }
   
     function setSwitchState(isEnabled, animate = false) {
@@ -29,7 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
       setSwitchState(isEnabled, true);
       
       chrome.storage.local.set({isEnabled: isEnabled}, function() {
-        
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           if (tabs[0] && tabs[0].url.includes("instagram.com")) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "toggleExtension", isEnabled: isEnabled})
